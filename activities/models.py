e4b048599cf318b1ea20c51399413dcd9b838ad8
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-06 00:43:41 +0300

    Adding urls

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
 # Create your models here.
+class DaysActivitie(models.Model):
+    date = models.DateField()
+    list_of_things = models.CharField(max_length=200)
+    productivity_range = models.PositiveIntegerField()
+    
