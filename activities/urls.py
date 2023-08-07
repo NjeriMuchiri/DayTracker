568c7a22694a9b8f375a8d7bb67680759b5667cf
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-07 14:30:52 +0300

    styling up my project

--- activities/urls.py
+++ activities/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = "activities"
 urlpatterns = [
-    path('home/', views.homeIndex, name="index"),
+    path('', views.homeIndex, name="index"),
     path('graph/', views.graph_representation, name="graph_representation")
 ]
